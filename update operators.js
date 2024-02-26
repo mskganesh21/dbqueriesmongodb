2104a671f6768fb39669b046242ef3f57844dc94
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-02-27 00:03:58 +0530

    Update update operators.js

--- update operators.js
+++ update operators.js
@@ -90,4 +90,112 @@ db.shoppingcart.updateMany({updateAt: {$exists: false}}, {
 //$, push , pull, addtoSet pullall, pop
 
 
+// array update methods
+
+//$ $push $pull $addToSet $pull $pullAll $pop
+
+//$push
+
+{"$push": {"key": "element"}}
+
+db.collection.update({cartId: 561}, {"$push": {"cart": "item1"}})
+
+ db.shoppingcart.update({"cartId": 325}, {"$push": {"cart": "item1"}})
+
+//each can add multiple elements once
+db.shoppingcart.update({"cartId": 325}, {"$push": {"cart" : {"$each" : ["item2", "item3"]}}})
+
+// if we are using push and array is not present then new array will be created
+
+//ADD TO SET OPERATOR 
+//prevents duplicate elements to be added to the array 
+
+
+db.shoppingcart.update({cartId: 561}, {"$addToSet": {"cart": "item1"}})
+
+
+db.shoppingcart.update({cartId: 561}, {"$addToSet": {"cart": {"$each": ["item2", "item3"]}}})
+
+
+
+//POP OPERATOR
+// remove the first/last element of the array
+// 1 last element is deleted
+//-1 then first element is deleted
+//  if only 1 element is preseent, then the element will be deleted and array will be preseent
+
+
+db.shoppingcart.update({cartId : 561}, {"$pop": {"cart": 1}})
+
+
+db.shoppingcart.update({index : 1}, {"$pop": {"cart": 1}})
+
+//$pull
+
+//remove selected element from the array or by condition
+
+db.shoppingcart.update({index: 1}, {"$pull": {"cart" : "item1"}})
+
+db.shoppingcart.update({index: 1}, {"$pull": {"cart" : {$gt: 400}}})
+
+//$pullAll
+
+// we have to specify the elements we need to remove in an array format and then we can remove it
+
+db.shoppingcart.update({index: 1}, {"$pullAll": {"cart": ["item1", "item2"]}})
+
+db.shoppingcart.update({index: 1}, {"$pull": {"cart": {"$in": ["item1", "item2"]}}})
+
+
+//positional operator $ operator
+
+{"$set": {"key.$" : "value"}}
+
+{"$set": {"key.$.field" : "value"}}
+
+db.shoppingcart.updateOne({cartId: 1, cart: "item2"}, {"$set": {
+  "cart.$": "updatedItem2"
+}})
+
+db.shoppingcart.updateOne({index: 1, cart: "item3"}, {"$unset": {"cart.$": 1}})
+
+
+// arrays with nested documents
+
+//without elemeMatch operator
+
+{cart: [{title: "TV", price: NumberInt(340), quantity : NumberInt(2)}, {title: "Phone", price: NumberInt(340), quantity : NumberInt(2)}]}
+
+db.shoppingcart.updateOne({"cart.title": "Phone"}, {
+  "$set": {
+    "cart.$.quantity": NumberInt(2)
+  }
+})
+
+db.shoppingcart.updateOne(
+  { "cartId": 456, "cart.title": "TV" },
+  { 
+    "$set": {
+      "cart.$.price": NumberInt(300),
+      "cart.$.quantity": NumberInt(10)
+    }
+  }
+)
+
+db.shoppingcart.updateOne({cart: {"$elemMatch": {title: "Phone", quantity: 4}}}, {"$set": {"cart.$.price": NumberInt(129)}})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
