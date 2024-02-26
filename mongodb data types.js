1f7df0ef3a46a48b5085aa5a6a7c49f2e83424e7
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-02-26 15:01:19 +0530

    Create mongodb data types.js

---
+++ mongodb data types.js
@@ -0,0 +1,25 @@
+//MONGODB DATA TYPES 
+
+// "string", "object", "array", "bool", "double", "date", "objectId"
+
+// _id: mandatory field
+/*
+can be assigned manually
+4 bytes date
+3 bytes machine _id
+2 bytes processid
+3 bytes counter Number
+24 hexa decimal Number
+ObjectId()
+*/
+
+// date format in mongodb
+
+//ISODate("")
+/*
+new Date()
+ISODate()
+
+Date()
+*/
+
